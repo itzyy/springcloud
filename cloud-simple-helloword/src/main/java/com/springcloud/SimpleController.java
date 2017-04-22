f536ad906c008e98fb8ad200f2eb94f36dc1cea5
itzyy <it_zyy@163.com>
2017-04-22 17:58:44 +0800

    20170422

---
+++ cloud-simple-helloword/src/main/java/com/springcloud/SimpleController.java
@@ -0,0 +1,21 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class SimpleController {
+
+	@ResponseBody
+	@RequestMapping(value="/")
+	public  String home(){
+		return "hello world";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleController.class, args);
+	}
+}
---
+++ cloud-simple-service/src/main/java/com/springcloud/CloudSimpleServiceApplication.java
@@ -0,0 +1,12 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSimpleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudSimpleServiceApplication.class, args);
+	}
+}
